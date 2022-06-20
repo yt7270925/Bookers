83ba76382b0a77859ec99a8ecee26602ec8fc161
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-20 20:23:34 +0000

    bookのdestroy機能作成まで　indexページレイアウトの途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to show_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -24,7 +24,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_path(book.id)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit'
-  patch 'books/:id' => 'books#update', as: 'update'
+  # get 'books' => 'books#index'
+  # get 'books' => 'books#new'
+  # post 'books' => 'books#create'
+  # get 'books/:id' => 'books#show', as: 'show'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit'
+  # patch 'books/:id' => 'books#update', as: 'update'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy'
   root :to => 'homes#top'
+
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
