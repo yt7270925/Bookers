2f07e2ff3285a73fb45665b12e117f0976e2cde3
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-21 09:31:03 +0000

    バリデーションがうまくいかない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,8 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     if book.save
       redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+      render :index
     end
   end
 
