d7801faa7eab79c0897fe7d54cec8b28631a6915
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-22 02:57:41 +0000

    createのバリデーション　終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,10 @@ class BooksController < ApplicationController
   # end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -28,14 +29,16 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
     end
   end
 
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-    redirect_to '/books', notice: 'Book was successfully destroyed.'
+      flash[:notice] = 'Book was successfully destroyed.'
+      redirect_to '/books'
     end
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+
   validates :title, presence: true
   validates :body, presence: true
+
 end
